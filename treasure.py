bc6cb9dbdceace0f57c5dbdd977272dad87f7534
Ahmed A <56705365+qaiys@users.noreply.github.com>
2021-02-04 01:03:34 -0500

    Update treasure.py

--- treasure.py
+++ treasure.py
@@ -3,15 +3,27 @@ import requests
 from lxml import html
 import shlex
 
-page = requests.get("https://letterboxd.com/philg2000/likes/films")
+
+class Film:
+    def __init__(self,name,num):
+        self.name = name
+        self.num = num 
+
+class Profile:
+    def __init__(self,name,movies=[]):  #this would all feel much nicer with just structs..
+        self.name = name
+        self.movies = movies
+
+profile = Profile("philg2000")
+
+page = requests.get("https://letterboxd.com/"+profile.name+"/likes/films")
 soup = BeautifulSoup(page.content, 'lxml')
 mydivs = soup.findAll("div", {"class": "poster film-poster really-lazy-load"})
 
-for div in mydivs:
-    print(shlex.split(str(div))[3])
-    print(shlex.split(str(div))[2])
-    print("--------")
+for div in mydivs: #TODO: get rid of shlex, very lazy hack. im pretty sure bs4 has something built in to do what im trying to do
+    temp = shlex.split(str(div))    
+    name = temp[3].split("/")[2]
+    num = temp[2].split("=")[1]
+    profile.movies.append(Film(name,num))
 
-
-
-
+print(profile.movies[0].name)
