ae44814225b4d9e7d959da60ea749854dbe7557c
Ahmed A <56705365+qaiys@users.noreply.github.com>
2021-02-05 00:21:47 -0500

    Update treasure.py
    can now see who liked what movie

--- treasure.py
+++ treasure.py
@@ -5,25 +5,41 @@ import shlex
 
 
 class Film:
-    def __init__(self,name,num):
+    def __init__(self,name,num,likers=[]):
         self.name = name
         self.num = num 
+        self.likers=likers
+    
+    def getLikers(self): #TODO: write some general parser that gets passed page and what youre looking for so we dont have to keep copy pasting this (or read bs4 docs? hmm)
+        page = requests.get("https://letterboxd.com/film/"+self.name+"/likes")
+        soup = BeautifulSoup(page.content, 'lxml')
+        mydivs = soup.findAll("div", {"class": "person-summary"})
+        for div in mydivs: 
+            temp = shlex.split(str(div))  
+            name = temp[4].split("/")[1]
+            self.likers.append(name)  
 
 class Profile:
-    def __init__(self,name,movies=[]):  #this would all feel much nicer with just structs..
+    def __init__(self,name,likes=[]):  #this would all feel much nicer with just structs..
         self.name = name
-        self.movies = movies
+        self.likes = likes
+    def getlikes(self):
+        page = requests.get("https://letterboxd.com/"+self.name+"/likes/films")
+        soup = BeautifulSoup(page.content, 'lxml')
+        mydivs = soup.findAll("div", {"class": "poster film-poster really-lazy-load"})
+        for div in mydivs: #TODO: get rid of shlex, very lazy hack. im pretty sure bs4 has something built in to do what im trying to do
+            temp = shlex.split(str(div))    
+            name = temp[3].split("/")[2]
+            num = temp[2].split("=")[1]
+            self.likes.append(Film(name,num))
 
-profile = Profile("philg2000")
 
-page = requests.get("https://letterboxd.com/"+profile.name+"/likes/films")
-soup = BeautifulSoup(page.content, 'lxml')
-mydivs = soup.findAll("div", {"class": "poster film-poster really-lazy-load"})
+#likes
+#seen
+#watchlist? 
 
-for div in mydivs: #TODO: get rid of shlex, very lazy hack. im pretty sure bs4 has something built in to do what im trying to do
-    temp = shlex.split(str(div))    
-    name = temp[3].split("/")[2]
-    num = temp[2].split("=")[1]
-    profile.movies.append(Film(name,num))
 
-print(profile.movies[0].name)
+profile = Profile("hahahahmed")
+profile.getlikes()
+profile.likes[3].getLikers() #wolf of wall street :3 
+print(profile.likes[3].likers)
