bc383fb7146babb12b00895e35daa2d25ce9085c
Philippe Gagné <philg2000@gmail.com>
2022-06-05 13:11:53 -0400

    fixing an oopsie

--- tboxd_backend/tboxd_backend/settings.py
+++ tboxd_backend/tboxd_backend/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^nf@*be)&w&_feixwwrlj$j-mlvll&2o(j_v-uk#e7tqh8i=^v'
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY") # this is to replace the secret key you cut away before
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
